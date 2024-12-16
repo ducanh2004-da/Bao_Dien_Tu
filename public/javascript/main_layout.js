421fc84fcdadf75ba3b3a5740d2b10cc1a55bb70
SAPAAAA <hoanglanbui1967@gmail.com>
2024-12-17 01:21:04 +0700

    fix: selecting different views

--- public/javascript/main_layout.js
+++ public/javascript/main_layout.js
@@ -2,6 +2,8 @@
 const slider = document.getElementById("slider");
 const btnLatest = document.getElementById("latest");
 const btnMostViews = document.getElementById("most-views");
+const divMostViews = document.getElementById("most-views-news");
+const divLatest = document.getElementById("latest-news");
 
 btnLatest.addEventListener("click", () => {
     slideTo("left");
@@ -17,6 +19,8 @@ function slideTo(position) {
         btnMostViews.classList.remove("active");
         btnLatest.classList.remove("inactive");
         btnLatest.classList.add("active");
+        divMostViews.style.display = "none";
+        divLatest.style.display = "block";
 
 
     } else if (position === "right") {
@@ -25,5 +29,7 @@ function slideTo(position) {
         btnMostViews.classList.add("active");
         btnLatest.classList.add("inactive");
         btnLatest.classList.remove("active");
+        divMostViews.style.display = "block";
+        divLatest.style.display = "none";
     }
 }
