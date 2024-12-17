daefa79a78e489e52cb7d8bfc2fca1b8157c7b68
SAPAAAA <hoanglanbui1967@gmail.com>
2024-12-18 04:19:49 +0700

    fix: missing authenticator for writer

--- routes/writer.js
+++ routes/writer.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/writer.js");
+const authMiddleware = require("../middlewares/auth");
 
-router.get("/", postController.showMainPage);
-router.get("/post-article", postController.showPostArticlePage);
-router.get("/my-article", postController.showMyArticlePage);
-router.get("/fix-article", postController.showFixArticlePage);
-router.post("/fix-article/submit", postController.submitFixArticle);
-router.get("/refuse-article", postController.showRefuseArticlePage);
-router.get("/category", postController.showCategoryPage);
-router.post("/submit-article", postController.submitArticle);
+router.get("/", authMiddleware.isUser, authMiddleware.isWriter, postController.showMainPage);
+router.get("/post-article", authMiddleware.isUser, authMiddleware.isWriter, postController.showPostArticlePage);
+router.get("/my-article", authMiddleware.isUser, authMiddleware.isWriter, postController.showMyArticlePage);
+router.get("/fix-article", authMiddleware.isUser, authMiddleware.isWriter, postController.showFixArticlePage);
+router.post("/fix-article/submit", authMiddleware.isUser, authMiddleware.isWriter, postController.submitFixArticle);
+router.get("/refuse-article", authMiddleware.isUser, authMiddleware.isWriter, postController.showRefuseArticlePage);
+router.get("/category", authMiddleware.isUser, authMiddleware.isWriter, postController.showCategoryPage);
+router.post("/submit-article", authMiddleware.isUser, authMiddleware.isWriter, postController.submitArticle);
 
 module.exports = router;
