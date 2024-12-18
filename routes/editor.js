84f60bea673e8e72bdfbc5932ce696e43f40753a
SAPAAAA <hoanglanbui1967@gmail.com>
2024-12-19 01:54:17 +0700

    fix: missing authenticator

--- routes/editor.js
+++ routes/editor.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const editorController = require("../controllers/editor.js");
+const authMiddleware = require("../middlewares/auth");
 
-router.get("/", editorController.showMainPage);
-router.get("/article", editorController.showArticlePage);
-router.get("/article-review", editorController.showArticleReview);
-router.get("/article-approved", editorController.articleApproved);
-router.post("/article-rejected", editorController.articleRejected);
+router.get("/", authMiddleware.isUser, authMiddleware.isEditor, editorController.showMainPage);
+router.get("/article", authMiddleware.isUser, authMiddleware.isEditor, editorController.showArticlePage);
+router.get("/article-review", authMiddleware.isUser, authMiddleware.isEditor, editorController.showArticleReview);
+router.post("/article-approved", authMiddleware.isUser, authMiddleware.isEditor, editorController.articleApproved);
+router.post("/article-rejected", authMiddleware.isUser, authMiddleware.isEditor, editorController.articleRejected);
 module.exports = router;
