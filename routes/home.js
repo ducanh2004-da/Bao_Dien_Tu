adcb760fbc26c6a89a09f4d7e4009615e24b120a
SAPAAAA <hoanglanbui1967@gmail.com>
2024-12-20 03:03:20 +0700

    fe: add category route

--- routes/home.js
+++ routes/home.js
@@ -6,6 +6,6 @@ const { validateQuery } = require('../validators/validators');
 router.get("/", homeController.showHomePage);
 router.get('/post/:id',homeController.showDetail);
 router.get('/search', validateQuery, homeController.search);
-// router.post('/post/like/:id',homeController.likePost);
+router.get('/category/:id', homeController.showCategory);
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,4 @@
 const express = require('express');
-//kiểm tra xem đã đăng nhập chưa
-const authMiddleware = require('../middlewares/auth');
-const AuthController = require('../controllers/auth');
 const profileController = require('../controllers/profile');
 const { validateQuery } = require('../validators/validators');
 
@@ -12,6 +9,7 @@ const MainController = require('../controllers/main');
 router.get('/',MainController.showMainPage);
 router.get('/post/:id', MainController.showDetail);
 router.post('/post/like/:id', MainController.likePost);
+router.get('/category/:id', MainController.showCategory);
 router.get('/search', validateQuery, MainController.search);
 router.get('/profile', profileController.show);
 router.get('/profile/edit', profileController.viewEdit);
